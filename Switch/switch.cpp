3091cea9e5405d5676d5689f6f629856e0371057
Kevin Specht <kspecht@mitre.org>
2024-06-15 01:13:13 -0400

    switch

---
+++ Switch/switch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    switch (2)
+    {
+    case 1:
+        std::cout << 1 << '\n';
+        break;
+    case 2:
+        std::cout << 2 << '\n'; // Execution begins here
+        [[fallthrough]]; // intentional fallthrough -- note the semicolon to indicate the null statement
+    case 3:
+        std::cout << 3 << '\n'; // This is also executed
+        break;
+    }
+
+    switch ('a')
+    {
+    case 'a': // if c is 'a'
+    case 'e': // or if c is 'e'
+    case 'i': // or if c is 'i'
+    case 'o': // or if c is 'o'
+    case 'u': // or if c is 'u'
+    case 'A': // or if c is 'A'
+    case 'E': // or if c is 'E'
+    case 'I': // or if c is 'I'
+    case 'O': // or if c is 'O'
+    case 'U': // or if c is 'U'
+	std::cout << "switch" <<'\n';
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
